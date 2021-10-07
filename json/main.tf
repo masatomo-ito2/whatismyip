e44a2408365ed131ce006ebebb4dc3fb757628ef
Masa Ito <masa@hashicorp.com>
2021-10-07 15:29:57 +0900

    json

--- json/main.tf
+++ json/main.tf
@@ -2,6 +2,6 @@ data "http" "my_ip" {
 	url = "https://api.ipify.org?format=json"
 }
 
-output "my_ip" {
-	value = "${data.http.my_ip.body}/32"
+output "ip" {
+	value = jsondecode( data.http.my_ip.body )
 }
